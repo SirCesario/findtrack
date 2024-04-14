eb1c25a61923bd0923d2a5ebc8f88c37dffe4392
Julio Cesar Cuevas Cardenas <jahve20@hotmail.com>
2024-04-13 21:39:15 -0500

    create project

---
+++ src/main/java/com/findtrack/JulioCesarCuevasCardenasApplication.java
@@ -0,0 +1,13 @@
+package com.findtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulioCesarCuevasCardenasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JulioCesarCuevasCardenasApplication.class, args);
+	}
+
+}
